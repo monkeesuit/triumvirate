6a2af48d6a0fd3f5d2f7e3fc2543abcbbfd7cd0a
monkeesuit <sam640@nyu.edu>
2017-02-27 05:34:53 -0500

    Update encrypt.c

--- decryption/source/encrypt.c
+++ decryption/source/encrypt.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
-#include "buffer.h"
+#include <string.h>
+#include <stdlib.h>
 
 #define SIZE 500
+
+#include "arrays.h"
+#include "buffer.h"
+#include "keygenerator.h"
 
 char *buffer[SIZE];
 
 int main() {
+  int i;
   
   read_into_buffer();       // Place message into gloabl buffer; from buffer.h
   
   printf("%s\n", buffer);
+  
+  for (i=0; i<27; i++) {
+    push(english_letters[i],frequency[i]);
+  }
+  display();
   
   return 0;
 }
