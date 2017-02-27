189d8b5f86a9f3121c92b68f4a7a79ad36f360e2
jettjaguar <helloworld_1020@yahoo.com>
2017-02-27 12:11:39 -0500

    Update encrypt.c

--- decryption/source/encrypt.c
+++ decryption/source/encrypt.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+//#include <string.h>     not needed
+//#include <stdlib.h>     not needed
 
-#define SIZE 500
+//#define SIZE 500        
 
-#include "arrays.h"
-#include "buffer.h"
-#include "keygenerator.h"
+#include "arrays.h"               //Had to define the function prototypes here
+void push(char k, int f);
+void display();
 
-char *buffer[SIZE];
+
+//char *buffer[SIZE];       Not putting anything into the buffer - Also should just be char *buffer or char buffer[SIZE] (no pointer)
 
 int main() {
   int i;
   
-  read_into_buffer();                         // Place message into gloabl buffer; from buffer.h
-  
-  printf("%s\n", buffer);
+//  read_into_buffer();                     //  Gives error since it's not currently reading anything     // Place message into gloabl buffer; from buffer.h
+                                              
+//  printf("%s\n", buffer);                 // Nothing to print
   
   for (i=0; i<27; i++) {
     push(english_letters[i],frequency[i]);    // Initialize Key Space; from keygenerator.h
