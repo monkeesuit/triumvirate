d8ff1abad6ecc7bca98e99f2037991ab88d8efa9
monkeesuit <sam640@nyu.edu>
2017-02-27 03:07:18 -0500

    Update keygenerator.h

--- decryption/source/keygenerator.h
+++ decryption/source/keygenerator.h
@@ -1,7 +1,7 @@
 #include 
 
 
-char  english_letters = {
+char  english_letters[27] = {
                              ' ', 'a', 'b', 'c', 'd',
                              'e', 'f', 'g', 'h', 'i',
                              'j', 'k', 'l', 'm', 'n',
