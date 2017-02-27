396b6e023f0f54925a21c0b152a66eb26366cb47
jettjaguar <helloworld_1020@yahoo.com>
2017-02-27 05:24:01 -0500

    Update keygenerator.h

--- decryption/source/keygenerator.h
+++ decryption/source/keygenerator.h
@@ -1,45 +1,48 @@
-#include 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void printList();
 
+char  english_letters[27] = { ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                                't', 'u', 'v', 'w', 'x', 'y', 'z'
+			    };
 
-char  english_letters[27] = {
-                            ' ', 'a', 'b', 'c', 'd',
-                            'e', 'f', 'g', 'h', 'i',
-                            'j', 'k', 'l', 'm', 'n',
-                            'o', 'p', 'q', 'r', 's',
-                            't', 'u', 'v', 'w', 'x',
-                            'y', 'z'
-                        	};
-
-int frequency[27] = {
-			  	 	19, 7, 1, 2, 4, 
-	 		  	 	10,  2, 2, 5, 6, 
-			  	 	1, 1, 3, 2, 6, 
-			  	 	6, 2, 1, 5, 5, 
-	             	7, 2, 1, 2, 1, 
-			  	 	2, 1
-		    		};
+int frequency[27] = { 19, 7, 1, 2, 4, 10,  2, 2, 5, 6, 1, 1, 3, 2, 6, 6, 2, 1, 5, 5, 7, 2, 1, 2, 1, 2, 1 };
 
 
 typedef struct KeySpaceElement {
-	char key[3];
+	char key;
 	int frequency;
 	char cipher_characters[60];
    	struct KeySpaceElement * next;
-}
+} keyspaceelement;
 
-KeySpaceElement * head = NULL;
-head = malloc(sizeof(KeySpaceElement));
-if (head == NULL) {
-    return 1;
-}
+keyspaceelement *head = NULL;
+//malloc(sizeof(keyspaceelement));
+//if (head == NULL) {
+//    return 1;
+//}``
 
-void push(KeySpaceElemnt ** head, char key, int frequency) {
-	KeySpaceElement * entry;
-    entry = malloc(sizeof(KeySpaceElement));
-
+void push( struct KeySpaceElement ** head, char key, int frequency) {
+	keyspaceelement * entry = malloc(sizeof(keyspaceelement));
     entry->key = key;
 	entry->frequency = frequency;
     entry->next = *head;
     *head = entry;
+    printList();
 }
+
+void printList() {
+    keyspaceelement *ptr = head;
+    printf("(%c, %d) ", ptr->key, ptr->frequency);
+}
+
+int main(void) {
+//     keyspaceelement *head = malloc(sizeof(keyspaceelement));
+//     if (head == NULL) {
+//     return 1;
+//  
+    for (int i = 0; i < 27; i++) {
+	   push(&head, english_letters[i], frequency[i]);
+    };
 }
