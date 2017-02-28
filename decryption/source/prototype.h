d322b5538698615dca82530c76c2e6543cfdc63c
monkeesuit <sam640@nyu.edu>
2017-02-28 02:14:42 -0500

    Update prototype.h

--- decryption/source/prototype.h
+++ decryption/source/prototype.h
@@ -1,8 +1,6 @@
 // keygenerator.c
-void push(char ch, int fr); 
-void push(char key, int frequency);
+struct KeySpaceElement * push(char key, int frequency);
 void display();
 
 // buffer.c
 int read_into_buffer();
-
