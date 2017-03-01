47905bd9e8b60fa5715b24fcf87da22e3e91f8bd
monkeesuit <sam640@nyu.edu>
2017-02-28 23:15:16 -0500

    Update keygenerator.c

--- decryption/source/keygenerator.c
+++ decryption/source/keygenerator.c
@@ -3,13 +3,10 @@
 #include <time.h>
 #include "arrays.h"
 
-int cipherdigits[106];
-
 // A KeySpaceElement consists of:
 //	a key: 		which is one of the english letters
 //	the frequency:	which is the frequency of the occurence of that letter
 //	values:		which is an array of digits that the key can encode to
-
 struct KeySpaceElement {
   char key;
   int frequency;
@@ -20,7 +17,6 @@ struct KeySpaceElement {
 
 // Initialize the linked-list
 struct KeySpaceElement *head = NULL;
-
 // Put an entry in the linked-list
 struct KeySpaceElement * push(char key, int frequency) {
   struct KeySpaceElement *entry;
@@ -38,49 +34,51 @@ struct KeySpaceElement * push(char key, int frequency) {
 // Display the linked-list
 void display() {
   int i;
-  struct KeySpaceElement *entry = head;
   
+  // walk the linked-list
+  struct KeySpaceElement *entry = head;
   while(entry != NULL) {
-    printf("(%c,%d)\n[", entry->key, entry->frequency);
-    
-    for(i=0; i<entry->frequency; i++) {
+    printf("%c\n", entry->key);               // display english letter
+    printf("[");
+    for(i=0; i<entry->frequency; i++) {       // display cipher digits
       printf("%d,",entry->values[i]);
     }
-    printf("]\n");
+    printf("]\n\n");
     
     entry = entry->next;
   }
 }
 
+// Key the linked-list 
 void pick() {
-  srand(time(NULL));
   int i, j, index, letter, digit, nop, keyed;
   
-  keyed=0;
-  nop=0;
+  keyed=0;                                               // keyed -> keeps tracks of elements that have been fully keyed
+  nop=0;                                                 // nop   -> keeps track of how many cipher digits have been chosen
   
-  while(keyed<27) {
-    index = rand() % 27;
+  // loop until all have been keyed
+  while(keyed<27) {                                      
+    index = rand() % 27;                                    // pick a random letter (<space>,a,b,c,...z)
     letter = english_letters[index];
     
-    struct KeySpaceElement *entry = head;
-    
-    while(entry != NULL) {
-      if (entry->key==letter) {
-        if(entry->remaining >0){
-          index = rand() % (106-nop);
-          digit = cipherdigits[index];
-          
-          entry->values[entry->frequency - entry->remaining] = digit;
-          
+    // walk the linked-list
+    struct KeySpaceElement *entry = head;        
+    while(entry != NULL) {                                  
+      if (entry->key==letter) {                               // if we find the entry that matches the random letter we chose,
+        if(entry->remaining >0){                              // and it is not keyed (remaining=0 => keyed +1)
           entry->remaining -= 1;
           if (entry->remaining == 0) {
             keyed += 1;
           }
           
-          cipherdigits[index] = cipherdigits[105-nop];
-          cipherdigits[105-nop] = '\0';
-          nop++;
+          index = rand() % (106-nop);                         // pick a number from cipher digits
+          digit = cipherdigits[index];
+          entry->values[entry->frequency - (entry->remaining+1)] = digit;   // and put it in the entries values array
+          
+          // pop the chosen digit from cipherdigits
+          cipherdigits[index] = cipherdigits[105-nop];    // this overwrites the chosen digit with the last non-nop digit in cipher digits
+          cipherdigits[105-nop] = '\0';                   // and then nop the copied digit at the end of the list
+          nop++;                                          // increment the nop variable to indicate that cipher digits is one unit smaller
           break;
         }
       }
@@ -90,6 +88,7 @@ void pick() {
   }
 }
 
+// Writes key to file key.txt
 void write_to_file() {
   int i;
   
@@ -100,8 +99,8 @@ void write_to_file() {
     exit(1);
   }
   
+  // walk the linked-list
   struct KeySpaceElement *entry = head;
-  
   while(entry != NULL) {
     fprintf(f,"%c\n",entry->key);
     for(i=0; i< entry->frequency; i++) {
@@ -115,13 +114,15 @@ void write_to_file() {
 
 int main(){
   int i;
+  int cipherdigits[106];
+  srand(time(NULL));
 
   for (i=0; i<106; i++) {
     cipherdigits[i]=i;
   }
   
   for (i=0; i<27; i++) {
-    push(english_letters[i],frequency[i]);    // Initialize Key Space; from keygenerator.h
+    push(english_letters[i],frequency[i]);    
   }
 
   pick();
