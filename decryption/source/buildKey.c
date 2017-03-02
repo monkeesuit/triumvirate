1155ad80a1344666db47208c5283309a2fc204c0
monkeesuit <sam640@nyu.edu>
2017-03-02 01:33:06 -0500

    Update buildKey.c

--- decryption/source/buildKey.c
+++ decryption/source/buildKey.c
@@ -7,7 +7,7 @@
 
 #define SIZE 500
 
-char buffer[SIZE];
+char data[SIZE];
 
 // Initialize the linked-lis
 struct KeySpaceElement *head = NULL;
@@ -23,26 +23,26 @@ struct KeySpaceElement {
 struct KeySpaceElement * push(char key, int frequency) {
   struct KeySpaceElement *entry;
   entry = malloc(sizeof(struct KeySpaceElement));
-
+  
   entry->key = key;
   entry->frequency = frequency;
   entry->remaining = frequency;
   entry->next = head;
   head = entry;
-
+  
   return head;
 }
 
-int read_into_buffer() {
-  extern char buffer[SIZE];
-  read(0, buffer, SIZE);
+int read_in_data() {
+  extern char data[SIZE];
+  read(0, data, SIZE);
   return 0;
 }
 
 // Display the linked-list
 void display() {
   int i;
-
+  
   // walk the linked-list
   struct KeySpaceElement *entry = head;
   while(entry != NULL) {
@@ -52,13 +52,14 @@ void display() {
       printf("%d,",entry->values[i]);
     }
     printf("]\n\n");
-
+    
     entry = entry->next;
   }
 }
 
 int main(void) {
-
+  
+  int index =0;
   int line_count = 0;
   int counter = 0;
   int left_bound = 0;
@@ -68,66 +69,82 @@ int main(void) {
   int lb = 0;
   int c = 0;
   char digit[4];
-  char key;
-
-
-  for (int i=0; i<27; i++) {
-      head = push(english_letters[i],frequency[i]);
-  }
-
-  display();
-  read_into_buffer();
+  char assign_key;
   
-  for (int i = 1; i < strlen(buffer); i++) {
-    if (buffer[i] != '\n') {
+  int i, j, k;
+  
+  struct KeySpaceElement *entry;
+  struct KeySpaceElement *head_a;
+  
+  
+  for (i=0; i<27; i++) {
+    head_a = push(english_letters[i],frequency[i]);
+  }
+  
+  read_in_data();
+  
+  for (i = 1; i < strlen(data); i++) {
+    if (data[i] != '\n') {
       counter += 1;
     }
-    if (buffer[i] == '\n') {
+    if (data[i] == '\n') {
       if (line_count % 2 == 0) {
         counter += 1;
         right_bound = left_bound + counter;
-        key = buffer[left_bound + 1];
+        assign_key = data[left_bound + 1];
+        
 
-        printf("\tKEY\t");
-        printf("LEFT BOUND = %d\t\t", left_bound);
-        printf("%c", key);
-        printf("\t\tRIGHT BOUND = %d", right_bound);
-        putchar('\n');
-
+        
         left_bound = right_bound;
         counter = 0;
         line_count += 1;
-              
+        
       } else {
         counter += 1;
         lb = left_bound;
         right_bound = left_bound + counter;
-        printf("\tVALUES\t");
-        printf("LEFT BOUND = %d\t\t", left_bound);
-        for (int j = left_bound + 1 ; j < right_bound; j++) {
-          if (buffer[j] != ',') {
+        
+        for (j = left_bound + 1 ; j < right_bound; j++) {
+          
+          if (data[j] != ',') {
             c += 1;
-          } else if (buffer[j] == ',') {
+            
+          } else if (data[j] == ',') {
             c += 1;
+            
             rb = lb + c;
+            
             memset(digit, 0, sizeof(digit));
-            for (int k = lb + 1; k < rb; k++) {
-              digit[k - lb + 1] = buffer[k];
+            for (k = lb + 1; k < rb; k++) {
+              digit[index] = data[k];
+              index += 1;
             }
-            printf("%s", digit);
-            putchar(',');
-
+            index = 0;
+            
             lb = rb;
             c = 0;
             value_index += 1;
+            
+            entry = head_a;
+            while(entry != NULL) {
+
+              if (entry->key == assign_key){
+                entry->values[ entry->frequency - entry->remaining ] = atoi(digit);
+                entry->remaining -= 1;
+                break;
+              }
+              
+              entry = entry->next;
+            }
+            
           }
         }
-        printf("\t\tRIGHT BOUND = %d", right_bound);
-        putchar('\n');
+        
         left_bound = right_bound;
         counter = 0;
         line_count +=1;
       }
     }
   }
+  display();
 }
